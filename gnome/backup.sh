703000c6717efa56cf024e13f5867d5654d36145
gutohertzog <guto.hertzog@gmail.com>
2026-07-08 21:51:21 -0300

    ajustes no conceallevel do vimrc e removido código legado

--- gnome/backup.sh
+++ gnome/backup.sh
@@ -2,11 +2,6 @@
 
 clear
 
-printf "Realizando backup das pastas do sistema\n"
-
-printf "Copiando arquivos\n"
-# $HOME/arch-hertzog/dotfiles/backup.sh
-
 # exporta as configurações do Gnome
 printf "Exportando as configurações do GNOME\n"
 dconf dump / | sed 's|/home/guto|/home/$USER|g' > $HOME/arch-hertzog/gnome/dotfiles/config/dconf/user-settings.conf
