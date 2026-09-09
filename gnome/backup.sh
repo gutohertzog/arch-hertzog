50cd1213111b42feeb2df46424fbadf7b646e377
gutohertzog <guto.hertzog@gmail.com>
2026-09-09 20:09:23 -0300

    user-settings.conf split
    - user-settings dividido em arquivos individuais
    - dconf-split.py divide
    - install-dconf.sh gerado automaticamente

--- gnome/backup.sh
+++ gnome/backup.sh
@@ -4,4 +4,4 @@ clear
 
 # exporta as configurações do Gnome
 printf "Exportando as configurações do GNOME\n"
-dconf dump / | sed 's|/home/guto|/home/$USER|g' > $HOME/arch-hertzog/gnome/dotfiles/config/dconf/user-settings.conf
+dconf dump / > $HOME/arch-hertzog/gnome/dotfiles/config/dconf/user-settings.conf
